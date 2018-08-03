19c761db850d8d0e18ea2b18d891e00fd6297657
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2018-08-03 15:29:17 +0200

    {'modified': ['product/ramdisk.mk']} cm-14.1
    Change-Id: Id94e1f2258b8a0a822c4688a527107032a66ba11

--- product/ramdisk.mk
+++ product/ramdisk.mk
@@ -1,16 +1,36 @@
 # Ramdisk
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/fstab.goldfish:root/fstab.goldfish \
+    $(LOCAL_PATH)/rootdir/enableswap.sh:root/enableswap.sh \
+    $(LOCAL_PATH)/rootdir/factory_init.connectivity.rc:root/factory_init.connectivity.rc \
+    $(LOCAL_PATH)/rootdir/factory_init.project.rc:root/factory_init.project.rc \
+    $(LOCAL_PATH)/rootdir/factory_init.rc:root/factory_init.rc \
     $(LOCAL_PATH)/rootdir/fstab.mt6757:root/fstab.mt6757 \
-    $(LOCAL_PATH)/rootdir/fstab.ranchu:root/fstab.ranchu \
+    $(LOCAL_PATH)/rootdir/init.aee.rc:root/init.aee.rc \
+    $(LOCAL_PATH)/rootdir/init.common_svc.rc:root/init.common_svc.rc \
+    $(LOCAL_PATH)/rootdir/init.connectivity.rc:root/init.connectivity.rc \
+    $(LOCAL_PATH)/rootdir/init.environ.rc:root/init.environ.rc \
+    $(LOCAL_PATH)/rootdir/init.epdg.rc:root/init.epdg.rc \
+    $(LOCAL_PATH)/rootdir/init.fingerprint.rc:root/init.fingerprint.rc \
+    $(LOCAL_PATH)/rootdir/init.modem.rc:root/init.modem.rc \
+    $(LOCAL_PATH)/rootdir/init.mt6757.rc:root/init.mt6757.rc \
+    $(LOCAL_PATH)/rootdir/init.mt6757.usb.rc:root/init.mt6757.usb.rc \
+    $(LOCAL_PATH)/rootdir/init.project.rc:root/init.project.rc \
     $(LOCAL_PATH)/rootdir/init.rc:root/init.rc \
-    $(LOCAL_PATH)/rootdir/init.recovery.usb.rc:root/init.recovery.usb.rc \
+    $(LOCAL_PATH)/rootdir/init.recovery.mt6757.rc:root/init.recovery.mt6757.rc \
+    $(LOCAL_PATH)/rootdir/init.rilproxy.rc:root/init.rilproxy.rc \
+    $(LOCAL_PATH)/rootdir/init.sensor.rc:root/init.sensor.rc \
+    $(LOCAL_PATH)/rootdir/init.trustkernel.rc:root/init.trustkernel.rc \
+    $(LOCAL_PATH)/rootdir/init.usb.configfs.rc:root/init.usb.configfs.rc \
+    $(LOCAL_PATH)/rootdir/init.usb.rc:root/init.usb.rc \
+    $(LOCAL_PATH)/rootdir/init.volte.rc:root/init.volte.rc \
+    $(LOCAL_PATH)/rootdir/init.zygote32.rc:root/init.zygote32.rc \
+    $(LOCAL_PATH)/rootdir/init.zygote64_32.rc:root/init.zygote64_32.rc \
+    $(LOCAL_PATH)/rootdir/meta_init.connectivity.rc:root/meta_init.connectivity.rc \
     $(LOCAL_PATH)/rootdir/meta_init.modem.rc:root/meta_init.modem.rc \
     $(LOCAL_PATH)/rootdir/meta_init.project.rc:root/meta_init.project.rc \
     $(LOCAL_PATH)/rootdir/meta_init.rc:root/meta_init.rc \
+    $(LOCAL_PATH)/rootdir/multi_init.rc:root/multi_init.rc \
     $(LOCAL_PATH)/rootdir/recovery.fstab:root/recovery.fstab \
-    $(LOCAL_PATH)/rootdir/ueventd.goldfish.rc:root/ueventd.goldfish.rc \
     $(LOCAL_PATH)/rootdir/ueventd.mt6757.rc:root/ueventd.mt6757.rc \
-    $(LOCAL_PATH)/rootdir/ueventd.ranchu.rc:root/ueventd.ranchu.rc \
     $(LOCAL_PATH)/rootdir/ueventd.rc:root/ueventd.rc \
     $(LOCAL_PATH)/rootdir/init/mediaserver.rc:system/etc/init/mediaserver.rc
