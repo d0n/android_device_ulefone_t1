90c279ce4654e8eb04cf0da9083f437b40451403
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2018-09-03 14:51:33 +0200

    {'modified': ['product/mt6757.mk', 'rootdir/Android.mk', 'rootdir/etc/recovery.fstab'], 'deleted': ['rootdir/etc/init.microtrust.rc', 'rootdir/init.connectivity.rc', 'rootdir/init.modem.rc', 'rootdir/init.mt6757.rc', 'rootdir/init.mt6757.usb.rc', 'rootdir/init.project.rc', 'rootdir/init.rilproxy.rc', 'rootdir/init.sensor.rc', 'rootdir/init.volte.rc']} cm-14.1

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,12 +1,12 @@
 LOCAL_PATH:= $(call my-dir)
-ifneq ($(filter x603,$(TARGET_DEVICE)),)
+ifneq ($(filter t1,$(TARGET_DEVICE)),)
 include $(CLEAR_VARS)
 
 # Device init files
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := enableswap.sh
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional eng userdebug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/enableswap.sh
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -14,7 +14,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := factory_init.connectivity.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional eng userdebug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := factory_init.connectivity.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -22,7 +22,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := factory_init.project.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional eng userdebug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/factory_init.project.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -30,7 +30,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := factory_init.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional eng userdebug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/factory_init.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -38,7 +38,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.mt6757
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional eng userdebug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.mt6757
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -46,7 +46,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.aee.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional eng userdebug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.aee.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -54,7 +54,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.common_svc.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional eng userdebug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.common_svc.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -62,7 +62,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.connectivity.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional eng userdebug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.connectivity.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -70,7 +70,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.microtrust.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional eng userdebug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.microtrust.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -78,7 +78,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.modem.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional eng userdebug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.modem.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -86,7 +86,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.mt6757.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional eng userdebug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.mt6757.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -94,7 +94,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.mt6757.usb.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional eng userdebug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.mt6757.usb.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -102,7 +102,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.project.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional eng userdebug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.project.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -110,7 +110,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.recovery.mt6757.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional eng userdebug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.recovery.mt6757.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -118,7 +118,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.rilproxy.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional eng userdebug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.rilproxy.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -126,7 +126,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.sensor.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional eng userdebug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.sensor.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -134,7 +134,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.usb.configfs.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional eng userdebug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.usb.configfs.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -142,7 +142,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.usb.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional eng userdebug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.usb.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -150,7 +150,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.volte.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional eng userdebug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.volte.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -158,7 +158,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.zygote32.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional eng userdebug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.zygote32.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -166,7 +166,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.zygote64_32.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional eng userdebug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.zygote64_32.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -174,7 +174,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := meta_init.connectivity.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional eng userdebug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/meta_init.connectivity.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -182,7 +182,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := meta_init.modem.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional eng userdebug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/meta_init.modem.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -190,7 +190,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := meta_init.project.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional eng userdebug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/meta_init.project.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -200,7 +200,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := meta_init.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional eng userdebug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/meta_init.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -208,7 +208,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := multi_init.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional eng userdebug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/multi_init.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -216,7 +216,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := recovery.fstab
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional eng userdebug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/recovery.fstab
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -224,7 +224,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.mt6757.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional eng userdebug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.mt6757.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -232,7 +232,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional eng userdebug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -240,7 +240,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.epdg.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional eng userdebug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.epdg.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
