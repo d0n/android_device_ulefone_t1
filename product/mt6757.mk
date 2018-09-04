96eaa590f4497496661f9ddd2e74052698823cdc
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2018-09-04 20:26:15 +0200

    {'modified': ['product/mt6757.mk']} cm-14.1
    Change-Id: Ib9046071571cb1a53571e29e76412aa29d7d2233

--- product/mt6757.mk
+++ product/mt6757.mk
@@ -34,33 +34,7 @@ PRODUCT_PACKAGES += \
 #
 ## Properly input of int.rc
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/factory_init.project.rc:root/factory_init.project.rc \
-    $(LOCAL_PATH)/rootdir/factory_init.rc:root/factory_init.rc \
-    $(LOCAL_PATH)/rootdir/fstab.mt6757:root/fstab.mt6757 \
-    $(LOCAL_PATH)/rootdir/init.modem.rc:root/init.modem.rc \
-    $(LOCAL_PATH)/rootdir/init.mt6757.rc:root/init.mt6757.rc \
-    $(LOCAL_PATH)/rootdir/init.mt6757.usb.rc:root/init.mt6757.usb.rc \
-    $(LOCAL_PATH)/rootdir/meta_init.modem.rc:root/meta_init.modem.rc \
-    $(LOCAL_PATH)/rootdir/meta_init.project.rc:root/meta_init.project.rc \
-    $(LOCAL_PATH)/rootdir/meta_init.rc:root/meta_init.rc \
-    $(LOCAL_PATH)/rootdir/ueventd.mt6757.rc:root/ueventd.mt6757.rc \
-    $(LOCAL_PATH)/rootdir/init/mediaserver.rc:system/etc/init/mediaserver.rc \
     $(LOCAL_PATH)/rootdir/etc/init.rc:root/init.rc
-#  $(LOCAL_PATH)/rootdir/enableswap.sh:recovery/root/enableswap.sh \
-#  $(LOCAL_PATH)/rootdir/factory_init.connectivity.rc:recovery/root/factory_init.connectivity.rc \
-#  $(LOCAL_PATH)/rootdir/factory_init.project.rc:recovery/root/factory_init.project.rc \
-#  $(LOCAL_PATH)/rootdir/factory_init.rc:recovery/root/factory_init.rc \
-#  $(LOCAL_PATH)/rootdir/fstab.mt6757:recovery/root/fstab.mt6757 \
-#  $(LOCAL_PATH)/rootdir/init.rc:recovery/root/init.rc \
-#  $(LOCAL_PATH)/rootdir/init.recovery.mt6757.rc:recovery/root/init.recovery.mt6757.rc \
-#  $(LOCAL_PATH)/rootdir/meta_init.connectivity.rc:recovery/root/meta_init.connectivity.rc \
-#  $(LOCAL_PATH)/rootdir/meta_init.modem.rc:recovery/root/meta_init.modem.rc \
-#  $(LOCAL_PATH)/rootdir/meta_init.project.rc:recovery/root/meta_init.project.rc \
-#  $(LOCAL_PATH)/rootdir/meta_init.rc:recovery/root/meta_init.rc \
-#  $(LOCAL_PATH)/rootdir/multi_init.rc:recovery/root/multi_init.rc \
-#  $(LOCAL_PATH)/rootdir/recovery.fstab:recovery/root/recovery.fstab \
-#  $(LOCAL_PATH)/rootdir/ueventd.mt6757.rc:recovery/root/ueventd.mt6757.rc \
-#  $(LOCAL_PATH)/rootdir/ueventd.rc:recovery/root/ueventd.rc \
 
 # Configurations
 PRODUCT_COPY_FILES += \
