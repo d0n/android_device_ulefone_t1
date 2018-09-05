8545dbe29a90c54d799fa14e8421384b7b7ab3da
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2018-09-05 14:27:58 +0200

    {'modified': ['Android.mk']} cm-14.1
    Change-Id: Idc650b8744d5ac4e73c7fa95645b6baea2d20510

--- Android.mk
+++ Android.mk
@@ -17,7 +17,6 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),t1)
-  include $(LOCAL_PATH)/BoardConfig.mk
   include $(call all-makefiles-under,$(LOCAL_PATH))
   $(shell mkdir -p $(OUT)/obj/busybox)
   $(shell mkdir -p $(OUT)/obj/KERNEL_OBJ/usr)
