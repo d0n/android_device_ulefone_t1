4227f1b71d27104f79a701e789a33fcdb118c4f2
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2018-09-05 22:12:28 +0200

    {'modified': ['Android.mk', 'wpa_supplicant_8_lib/Android.mk']} cm-14.1
    Change-Id: If62f45a86d47b8ae2db3dcdac11dc11090494649

--- Android.mk
+++ Android.mk
@@ -15,10 +15,8 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+$(shell mkdir -p $(OUT)/obj/busybox)
+$(shell mkdir -p $(OUT)/obj/KERNEL_OBJ/usr)
 
-ifeq ($(TARGET_DEVICE),t1)
-  include $(call all-makefiles-under,$(LOCAL_PATH))
-  $(shell mkdir -p $(OUT)/obj/busybox)
-  $(shell mkdir -p $(OUT)/obj/KERNEL_OBJ/usr)
-endif
+include $(call all-makefiles-under,$(LOCAL_PATH))
 include $(LOCAL_PATH)/BoardConfig.mk
