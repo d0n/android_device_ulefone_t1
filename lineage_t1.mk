d049262a7003283dd1fccfef3ff5daf3e9613140
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2018-09-06 21:34:20 +0200

    {'modified': ['BoardConfig.mk'], 'deleted': ['full_t1.mk'], 'added': ['lineage_t1.mk']} lineage-15.1
    Change-Id: I8a212c85df19b6ba4225bc65d612e9ba84c9cd81

--- lineage_t1.mk
+++ lineage_t1.mk
@@ -4,22 +4,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from hardware-specific part of the product configuration
-$(call inherit-product, device/ulefone/metal/device.mk)
+$(call inherit-product, device/ulefone/t1/device.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := Metal
+PRODUCT_RELEASE_NAME := T1
 
-PRODUCT_DEVICE := metal
-PRODUCT_NAME := full_metal
+PRODUCT_DEVICE := t1
+PRODUCT_NAME := lineage_t1
 PRODUCT_BRAND := Ulefone
-PRODUCT_MODEL := Metal
+PRODUCT_MODEL := T1
 PRODUCT_MANUFACTURER := Ulefone
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 # Boot animation
-TARGET_SCREEN_HEIGHT      := 1280
-TARGET_SCREEN_WIDTH       := 720
-TARGET_BOOTANIMATION_NAME := 720
+TARGET_SCREEN_HEIGHT      := 1920
+TARGET_SCREEN_WIDTH       := 1080
+TARGET_BOOTANIMATION_NAME := 1080
 
-PRODUCT_DEFAULT_LANGUAGE := en
-PRODUCT_DEFAULT_REGION   := US
+PRODUCT_DEFAULT_LANGUAGE := de
+PRODUCT_DEFAULT_REGION   := DE
