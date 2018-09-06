be8f99fd25aff41e6879966ed3ca265440cd8ee7
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2018-09-06 16:35:23 +0200

    {'modified': ['device.mk']} cm-15.1
    Change-Id: I18fe1deb549fda2844e72c3fbde4bfb10361a92c

--- device.mk
+++ device.mk
@@ -14,32 +14,13 @@
 # limitations under the License.
 #
 
-LOCAL_PATH := device/ulefone/t1
-
-# Kernel information
-BOARD_KERNEL_PAGESIZE := 2048
-BOARD_NAME := 1509953733
-BOARD_KERNEL_OFFSET := 0x00008000
-BOARD_RAMDISK_OFFSET := 0x40ef8000
-BOARD_TAGS_OFFSET := 0x03f88000
-BOARD_MKBOOTIMG_ARGS := --cmdline bootopt=64S3,32N2,64N2 --base 0x40078000 --kernel_offset $(BOARD_KERNEL_OFFSET) --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_TAGS_OFFSET) --board $(BOARD_NAME) --pagesize $(BOARD_KERNEL_PAGESIZE)
-TARGET_IS_64_BIT := true
-MTK_K64_SUPPORT := yes
-TARGET_KERNEL_ARCH := arm64
-
-# Kernel properties
-#TARGET_KERNEL_SOURCE := kernel/ulefone/t1
-TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilts/kernel
-TARGET_KERNEL_CONFIG := lineage_t1_defconfig
-BOARD_KERNEL_IMAGE_NAME := Image.gz
-MTK_APPENDED_DTB_SUPPORT := yes
-
 ## device overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
 
 ## (2) Also get non-open-source specific aspects if available
 $(call inherit-product-if-exists, device/ulefone/t1/product/mt6757.mk)
 $(call inherit-product-if-exists, vendor/ulefone/t1/t1-vendor.mk)
 
 $(call inherit-product, device/ulefone/mt6757-common/mt6757-common.mk)
-
