172492e415278caa5e0c318b263bfd7910e29af4
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2018-11-12 14:19:45 +0100

    {'modified': ['BoardConfig.mk', 'setup-vendor-blobs.sh'], 'added': ['policy']} cm-14.1
    Change-Id: I9c28a558ede6eff1ddc8a6cf2efb4aca7b21581b

--- setup-vendor-blobs.sh
+++ setup-vendor-blobs.sh
@@ -21,7 +21,7 @@ for I in $(find bin/ vendor/bin -type f) ;do
   printf "include \$(BUILD_PREBUILT)\n\n" >>$AMAKE
   printf "  ${BIN} \\\\\n" >>$VMAKE
 done
-for I in $(find * -xtype f -not -name *.odex -and -not -name *.apk) ;do
+for I in $(find * -xtype f) ;do
   #grep -qs $I $AMAKE && continue
   if [ "$(dirname $I)" == "smartpa_params" ] ;then
     I="etc/audio_param/$(basename $I)"
