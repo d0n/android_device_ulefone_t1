cead821df72b7cddf338321745302619609e66d9
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2018-11-13 02:27:03 +0100

    {'modified': ['setup-vendor-blobs.sh']} cm-14.1
    Change-Id: I44169c09bb7ba9712b3194a18827719e510c5e69

--- setup-vendor-blobs.sh
+++ setup-vendor-blobs.sh
@@ -8,7 +8,7 @@ BMAKE=${OUTDIR}/$DEVICE-vendor-blobs.mk
 VMAKE=${OUTDIR}/VendorBoardConfig.mk
 
 cd $OUTDIR/proprietary >/dev/null
-for I in $(find bin/ vendor/bin -type f) ;do
+for I in $(find bin/ vendor/bin -type f -not -name "*.apk") ;do
   grep -qs $I $AMAKE && continue
   grep -qs $I $ddir/blackbins.lst && continue
   echo $I
