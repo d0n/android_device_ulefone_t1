759ecf68853d54f03a369cd6c0ce5d6adfe5e0ea
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2018-11-15 06:10:14 +0100

    {'modified': ['BoardConfig.mk', 'product/mt6757.mk'], 'deleted': ['recovery_updater/Android.mk', 'recovery_updater/recovery_updater.cpp']} cm-14.1
    Change-Id: Ie41d311337f22911bd67b968425ae13d7ca46c0b

--- product/mt6757.mk
+++ product/mt6757.mk
@@ -410,11 +410,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
   $(LOCAL_PATH)/configs/keylayout/ACCDET.kl:system/usr/keylayout/ACCDET.kl
 
-
-#PRODUCT_SYSTEM_SERVER_JARS += com.cyanogenmod.keyhandler
-
-#$(call add-product-dex-preopt-module-config,com.cyanogenmod.keyhandler,disable)
-
 PRODUCT_COPY_FILES += \
   frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
   frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
